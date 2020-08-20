c2764f02f8df9152b3bb7482cf7e1e4d63bee7d9
Nick Lavery <tuneboynick@gmail.com>
2020-08-19 21:22:47 -0400

    Walkthrough

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import Calculator from './Calculator';
-import Card from './Menu';
+import './Calculator';
+import Calculator from './Calculator';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Card
-        </p>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  return (
+    <div className="app calculator">
+
+      <header>React Calculator</header>
+
+      <Calculator />
+
+
     </div>
-  );
+  )
 }
+
+
+
 
 export default App;
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,35 +1,103 @@
 import React from 'react';
-import Card from './Card';
-
-const operators = {
-    add: '+',
-    substract: '-',
-    multiply: 'x',
-    divide: '/'
-};
 
 
+class Calculator extends React.Component {
 
-function add() {
-    let numberOne = parseInt();
-    let numberTwo = parseInt();
+    constructor() {
+        super();
 
-    return numberOne + numberTwo;
+        this.state = {
+            firstOperand: 0,
+            operator: '+',
+            secondOperand: 0,
+            result: '0'
+        }
+
+        // "this" only exists for event handlers if we include 
+        // lines like this for each specicfic event handler
+        // this.onFirstOperandChange = this.onFirstOperandChange.bind(this)
+        // this.calculate = this.calculate.bind(this)
+
+    }
+
+    /* 
+    Event target is the element that had the event,
+    in this case the firstOperand input had a "change" event.
+     */
+
+    onFirstOperandChange = (event) => {
+        const newValue = event.target.value
+        this.setState({
+            firstOperand: newValue
+        })
+
+    }
+
+    onSecondOperandChange = (event) => {
+        const secondOperand = event.target.value;
+        this.setState({
+            secondOperand
+        })
+
+    }
+
+    onOperatorChange = (event) => {
+        const operator = event.target.value;
+        this.setState({
+            operator
+        })
+    }
+
+    calculate = (event) => {
+        let result = 0;
+        const { firstOperand, secondOperand, operator } = this.state;
+
+        switch (operator) {
+            case '+':
+                result = parseInt(firstOperand) + parseInt(secondOperand)
+                break;
+            case '-':
+                result = parseInt(firstOperand) - parseInt(secondOperand)
+                break;
+            case '*':
+                result = parseInt(firstOperand) * parseInt(secondOperand)
+                break;
+            case '/':
+                result = parseInt(firstOperand) / parseInt(secondOperand)
+                break;
+        }
+
+        this.setState({
+            result
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <input type="number" onChange={this.onFirstOperandChange} />
+
+                <select onChange={this.onOperatorChange} >
+                    <option>+</option>
+                    <option>-</option>
+                    <option>*</option>
+                    <option>/</option>
+                </select>
+
+                <input type="number" onChange={this.onSecondOperandChange} />
+
+                <button onClick={this.calculate}>=</button>
+
+                <input value={this.state.result} type="text" disabled />
+            </div>
+        );
+    }
 }
 
-function subract() {
-
-}
-
-function multiply() {
-
-}
-
-function divide() {
-
-}
 
 
 
 
 
+export default Calculator;
